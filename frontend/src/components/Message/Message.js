bf0fcfe71cc9573edb86b75c780fcc1aad299e72
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-04 13:03:39 -0300

    Criando componente de mensagem

---
+++ frontend/src/components/Message/Message.js
@@ -0,0 +1,11 @@
+import './Message.css';
+
+const Message = ({ message, type }) => {
+    return (
+        <div className={`message ${type}`}>
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default Message;
