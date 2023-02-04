9dc41fdd42fb5515614e09fb750ca0def0ae320b
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-04 01:58:13 -0300

    Criando componentes de Navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+//Components
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={ <Login /> } />
-          <Route path='/register' element={ <Register /> } />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import './Navbar.css';
+
+//Components 
+import { NavLink, Link } from 'react-router-dom';
+import {
+    BsSearch,
+    BsHouseDoorFill,
+    BsFillPersonFill,
+    BsFillCameraFill
+} from 'react-icons/bs';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="nav">
+                <Link to="/">ReactGram</Link>
+                <form>
+                    <BsSearch />
+                    <input type="text" />
+                </form>
+                <ul className="nav-links">
+                    <li><NavLink to="/"><BsHouseDoorFill /></NavLink></li>
+                    <li><NavLink to="/login">Entrar</NavLink></li>
+                    <li><NavLink to="/register">Cadastrar</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
