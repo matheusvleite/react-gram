c3549e04c7fb37fbe3ee9fdad654f2b977b6b7c8
Matheus Victor <matheusvictor2000@gmail.com>
2023-02-07 12:30:37 -0300

    Criando service e slice de autalizar usuario

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -18,6 +18,21 @@ export const profile = createAsyncThunk(
 
         return data;
     }
+);
+
+export const updateProfile = createAsyncThunk(
+    "user/update",
+    async (user, thunkAPI) => {
+        const token = thunkAPI.getState.auth.user.token
+
+        const data = await userService.updateProfile(user, token)
+
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
 )
 
 export const userSlice = createSlice({
@@ -27,22 +42,31 @@ export const userSlice = createSlice({
         resetMessage: (state) => {
             state.message = null;
         }
-    }, 
+    },
     extraReducers: (builder) => {
         builder
-        .addCase(profile.pending, (state) => {
-            state.loading = true;
-            state.error = false
-        }).addCase(profile.fulfilled, (state, action) => {
-            state.loading = false;
-            state.success = true;
-            state.error = null;
-            state.user = action.payload;
-        }).addCase(profile.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-            state.user = null;
-        })
+            .addCase(profile.pending, (state) => {
+                state.loading = true;
+                state.error = false
+            }).addCase(profile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.user = action.payload;
+            }).addCase(updateProfile.pending, (state) => {
+                state.loading = true;
+                state.error = false
+            }).addCase(updateProfile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.user = action.payload;
+                state.message = "Usuário atualizado com sucesso!"
+            }).addCase(updateProfile.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.user = null;
+            })
     }
 })
 
